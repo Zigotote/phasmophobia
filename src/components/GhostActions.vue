38ba35a02f17cd7f1c2fb84b0faad35dedd15bad
Zigotote <oce88b@gmail.com>
2021-06-30 21:09:26 +0200

    Add: emf control

---
+++ src/components/GhostActions.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="col-9 border border-dark border-2 rounded">
+    <div class="row m-1">
+      <img
+        class="col-3 rounded-pill"
+        :class="!state.power ? 'border border-danger border-3' : ''"
+        alt="Lumière éteinte"
+        :src="`${publicPath}assets/power_off.svg`"
+      />
+      <div class="form-check form-switch col-3">
+        <input
+          class="form-check-input mx-auto mt-5"
+          type="checkbox"
+          id="switch"
+          :disabled="!state.huntingAvailable"
+          @change="togglePower"
+        />
+      </div>
+      <img
+        class="col-3 rounded-pill"
+        :class="state.power ? 'border border-danger border-3' : ''"
+        :src="`${publicPath}assets/power_on.svg`"
+        alt="Lumière allumée"
+      />
+    </div>
+    <div class="row m-1">
+      <div class="row">
+        <h5 class="text-center mb-5">Variation de l'EMF</h5>
+      </div>
+      <div class="row">
+        <div class="form-check col" v-for="(emf, index) in emfs" :key="index">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="emf"
+            :checked="index == 1"
+            :disabled="!state.huntingAvailable"
+            @click="() => changeEmf(index)"
+          />
+          <label class="form-check-label" for="emf">
+            {{ emf }}
+          </label>
+        </div>
+      </div>
+      <button
+        type="button"
+        class="btn btn-danger col m-1"
+        :disabled="!state.huntingAvailable"
+        @click="startHunt"
+      >
+        <img alt="Mode chasse" :src="`${publicPath}assets/target.svg`" />
+        Chasser
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  name: "GhostActions",
+  setup() {
+    const publicPath = process.env.BASE_URL;
+    const state = reactive({ power: false, huntingAvailable: true });
+    const togglePower = () => {
+      state.power = !state.power;
+      console.log(state.power);
+    };
+    const emfs = ["Rare", "Occasionnel", "Souvent"];
+    const changeEmf = (value) => {
+      console.log(value);
+    };
+    const startHunt = () => {
+      console.log("Start hunt");
+      state.huntingAvailable = false;
+      setTimeout(() => {
+        state.huntingAvailable = true;
+      }, 20000);
+    };
+    return { publicPath, state, togglePower, changeEmf, emfs, startHunt };
+  },
+};
+</script>
+
+<style scoped>
+#switch {
+  width: 100%;
+  height: 50%;
+}
+</style>
--- src/views/GhostHome.vue
+++ src/views/GhostHome.vue
@@ -2,61 +2,19 @@
   <div class="container">
     <h1 class="text-center mb-5">Vue du fantôme</h1>
     <div class="row">
-      <div class="col-9">
-        <div class="row">
-          <img
-            class="col-3 rounded-pill"
-            :class="!state.power ? 'border border-danger border-3' : ''"
-            alt="Lumière éteinte"
-            :src="`${publicPath}assets/power_off.svg`"
-          />
-          <div class="form-check form-switch col-3">
-            <input
-              class="form-check-input mx-auto mt-5"
-              type="checkbox"
-              id="switch"
-              @change="togglePower"
-            />
-          </div>
-          <img
-            class="col-3 rounded-pill"
-            :class="state.power ? 'border border-danger border-3' : ''"
-            alt="Lumière allumée"
-            :src="`${publicPath}assets/power_on.svg`"
-          />
-        </div>
-      </div>
+      <ghost-actions />
       <div class="col-3"></div>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
+import GhostActions from "../components/GhostActions.vue";
 
 export default {
   name: "GhostHome",
-  setup() {
-    const publicPath = process.env.BASE_URL;
-    const state = reactive({ power: false });
-    const togglePower = () => {
-      state.power = !state.power;
-      console.log(state.power);
-    };
-    return { publicPath, state, togglePower };
-  },
+  components: { GhostActions },
 };
 </script>
 
-<style scoped>
-#switch {
-  width: 100%;
-  height: 50%;
-}
-#selected {
-  background-color: pink;
-}
-#unselected {
-  background-color: black;
-}
-</style>
+<style scoped></style>
