dd37b08c677254ac75cf7bc87b5955d97d768a9c
Zigotote <oce88b@gmail.com>
2021-06-30 21:36:43 +0200

    Add: group data

--- src/views/GhostHome.vue
+++ src/views/GhostHome.vue
@@ -2,20 +2,23 @@
   <div class="container">
     <h1 class="text-center mb-5">Vue du fantôme</h1>
     <div class="row">
-      <div class="col-9 border border-dark border-2 rounded">
+      <div class="col-8 border border-dark border-2 rounded m-1">
         <ghost-actions />
       </div>
-      <div class="col-3"></div>
+      <div class="col-3 border border-dark border-2 rounded m-1">
+        <group-data />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import GhostActions from "../components/GhostActions.vue";
+import GroupData from "../components/GroupData.vue";
 
 export default {
   name: "GhostHome",
-  components: { GhostActions },
+  components: { GhostActions, GroupData },
 };
 </script>
 
