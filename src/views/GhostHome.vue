5b18c9b8280e4ec596f6b16001fac33de77a1107
Zigotote <oce88b@gmail.com>
2021-06-30 19:33:42 +0200

    Add: light control

--- src/views/GhostHome.vue
+++ src/views/GhostHome.vue
@@ -1,16 +1,62 @@
 <template>
   <div class="container">
-    Hello vue !
-    <img alt="Vue logo" :src="`${publicPath}assets/book.svg`" />
+    <h1 class="text-center mb-5">Vue du fantôme</h1>
+    <div class="row">
+      <div class="col-9">
+        <div class="row">
+          <img
+            class="col-3 rounded-pill"
+            :class="!state.power ? 'border border-danger border-3' : ''"
+            alt="Lumière éteinte"
+            :src="`${publicPath}assets/power_off.svg`"
+          />
+          <div class="form-check form-switch col-3">
+            <input
+              class="form-check-input mx-auto mt-5"
+              type="checkbox"
+              id="switch"
+              @change="togglePower"
+            />
+          </div>
+          <img
+            class="col-3 rounded-pill"
+            :class="state.power ? 'border border-danger border-3' : ''"
+            alt="Lumière allumée"
+            :src="`${publicPath}assets/power_on.svg`"
+          />
+        </div>
+      </div>
+      <div class="col-3"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
+
 export default {
   name: "GhostHome",
   setup() {
     const publicPath = process.env.BASE_URL;
-    return { publicPath };
+    const state = reactive({ power: false });
+    const togglePower = () => {
+      state.power = !state.power;
+      console.log(state.power);
+    };
+    return { publicPath, state, togglePower };
   },
 };
 </script>
+
+<style scoped>
+#switch {
+  width: 100%;
+  height: 50%;
+}
+#selected {
+  background-color: pink;
+}
+#unselected {
+  background-color: black;
+}
+</style>
